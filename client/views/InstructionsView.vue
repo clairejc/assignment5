c0df3c75af6f37c1e7e8936b9425485136104b28
Claire <clairejc@mit.edu>
2024-10-26 18:46:06 -0400

    remove InstructionsView

--- client/views/InstructionsView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import InstructionsListComponent from "@/components/Instructions/InstructionsListComponent.vue";
-</script>
-
-<template>
-  <main class="column">
-    <InstructionsListComponent />
-  </main>
-</template>
